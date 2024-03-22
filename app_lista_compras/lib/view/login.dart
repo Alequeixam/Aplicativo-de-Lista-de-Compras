7858e7af44fc6f42aeac779e7a0a821024f07e48
Alex Santana <alequeixam@gmail.com>
2024-03-22 12:39:58 -0300

    TextFormField email senha

--- app_lista_compras/lib/view/login.dart
+++ app_lista_compras/lib/view/login.dart
@@ -8,16 +8,50 @@ class Login extends StatefulWidget {
 }
 
 class _PrincipalViewState extends State<Login> {
+  //Identificador do formulário
+  var formKey = GlobalKey<FormState>();
+
+  //Controladores dos campos de texto
+  var txtValor1 = TextEditingController();
+  var txtValor2 = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login"),
-        backgroundColor: Colors.deepPurple[200],
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(20),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Login"),
+          backgroundColor: Colors.deepPurple[200],
+        ),
+        body: Center(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Form(
+              key: formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      label: Text('E-mail'),
+                      hintText: 'nome@email.com',
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      label: Text('Senha'),
+                      hintText: 'Digite sua senha',
+                    ),
+                  ),
+                  SizedBox(height: 60),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Entrar"),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
