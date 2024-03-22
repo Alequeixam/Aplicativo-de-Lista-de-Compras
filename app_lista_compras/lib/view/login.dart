ad1189696fcf8bc14fdaeebc0568b64e96d85fc7
Alex Santana <alequeixam@gmail.com>
2024-03-22 12:55:03 -0300

    Email validator implemented

--- app_lista_compras/lib/view/login.dart
+++ app_lista_compras/lib/view/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -9,11 +10,11 @@ class Login extends StatefulWidget {
 
 class _PrincipalViewState extends State<Login> {
   //Identificador do formulário
-  var formKey = GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
 
   //Controladores dos campos de texto
-  var txtValor1 = TextEditingController();
-  var txtValor2 = TextEditingController();
+  final _emailController = TextEditingController();
+  final _senhaController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +27,7 @@ class _PrincipalViewState extends State<Login> {
           child: Padding(
             padding: EdgeInsets.all(20),
             child: Form(
-              key: formKey,
+              key: _formKey,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -35,6 +36,13 @@ class _PrincipalViewState extends State<Login> {
                       label: Text('E-mail'),
                       hintText: 'nome@email.com',
                     ),
+                    validator: (email) {
+                      if (email == null || email.isEmpty) {
+                        return 'Por favor, insira um e-mail';
+                      } else if(!EmailValidator.validate(email)) {
+                        return 'Digite um endereço de e-mail válido!';
+                      }
+                    },
                   ),
                   SizedBox(height: 20),
                   TextFormField(
@@ -42,10 +50,22 @@ class _PrincipalViewState extends State<Login> {
                       label: Text('Senha'),
                       hintText: 'Digite sua senha',
                     ),
+                    validator: (senha) {
+                      if (senha == null || senha.isEmpty) {
+                        return 'Por favor, insira uma senha';
+                      } else if (senha.length < 6) {
+                        return 'A senha deve conter no mínimo 6 dígitos!';
+                      }
+                    },
                   ),
+                  
                   SizedBox(height: 60),
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+
+                      }
+                    },
                     child: Text("Entrar"),
                   )
                 ],
