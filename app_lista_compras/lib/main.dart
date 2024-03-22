b3feab616e227e3c79744860e4b61dc04b3b6e6e
ALEX HENRIQUE SANTANA <2840482223014@fatec2.local>
2024-03-21 21:47:48 -0300

    Add Principal view

--- app_lista_compras/lib/main.dart
+++ app_lista_compras/lib/main.dart
@@ -1,7 +1,14 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -9,11 +16,31 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Meu App',
+      home: PrincipalView(),
+    );
+  }
+}
+
+class PrincipalView extends StatefulWidget {
+  const PrincipalView({super.key});
+
+  @override
+  State<PrincipalView> createState() => _PrincipalViewState();
+}
+
+class _PrincipalViewState extends State<PrincipalView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Compras"),
+        backgroundColor: Colors.deepPurple[200],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
       ),
     );
   }
---
+++ app_lista_compras/lib/model/item.dart
@@ -0,0 +1,7 @@
+class Item{
+  final String nome;
+  final int qtde;
+
+
+  Item(this.nome, this.qtde);
+}
