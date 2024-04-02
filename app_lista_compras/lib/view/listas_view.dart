3173b55c4e401d538c4f5e80d0a01ca5ca6ba28b
Alex Santana <alequeixam@gmail.com>
2024-04-02 17:29:18 -0300

    barra de busca

--- app_lista_compras/lib/view/listas_view.dart
+++ app_lista_compras/lib/view/listas_view.dart
@@ -13,16 +13,42 @@ class _ListasViewState extends State<ListasView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFEEEFF5),
       appBar: AppBar(
+        backgroundColor: Color(0xFFEEEFF5),
         centerTitle: true,
         title: Text('Listas de Compras'),
       ),
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              Text('abrir'),
-            ],
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          child: Center(
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.all(0),
+                        prefixIcon: Icon(
+                          Icons.search,
+                          size: 20,
+                        ),
+                        prefixIconConstraints: BoxConstraints(
+                          maxHeight: 20,
+                          minWidth: 20,
+                        ),
+                        border: InputBorder.none,
+                        hintText: 'Buscar',
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
