4053a277d0a6bae5f2edbcd73fb894bd084bcc09
Alex Santana <alex.santana01@fatec.sp.gov.br>
2024-04-04 21:34:51 -0300

    read, create e delete implementado

--- app_lista_compras/lib/model/item.dart
+++ app_lista_compras/lib/model/item.dart
@@ -1,7 +1,18 @@
 class Item {
-  final String nome;
-  final int qtde;
+  final String? nome;
+  final String? qtde;
+  bool isDone;
 
 
-  Item(this.nome, this.qtde);
+  Item({required this.nome, required this.qtde, this.isDone = false});
+
+  static List<Item> listaTeste(){
+    return [
+      Item(nome: "Pao de aio", qtde:  "10"),
+      Item(nome: "Picanha", qtde: "3kg"),
+      Item(nome: "Linguiça", qtde: "5kg"),
+      Item(nome: "Cerveja X", qtde: "10 far"),
+
+    ];
+  }
 }
--- app_lista_compras/lib/view/itens_view.dart
+++ app_lista_compras/lib/view/itens_view.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:app_lista_compras/view/criar_lista_view.dart';
+import 'package:app_lista_compras/model/item.dart';
 import 'package:flutter/material.dart';
 import '../model/lista.dart';
 
@@ -16,16 +16,17 @@ class _ItensState extends State<Itens> {
   //Identificador do formulário
   final _formKey = GlobalKey<FormState>();
 
-  final TextEditingController _nomeLista = TextEditingController();
+  final TextEditingController _nomeItem = TextEditingController();
+  final TextEditingController _qtdItem = TextEditingController();
 
-  List<Lista> listasTeste = Lista.listaTeste();
-  final List<Lista> _listas = List.empty(growable: true);
+  List<Item> itensTeste = Item.listaTeste();
+  List<Item> _itens = List.empty(growable: true);
 
-  /*@override
+  /* @override
   void initState() {
-    _listas.add(Lista(nome: "AAAA", itens: List.empty()));
+    _itens.add(Item.listaTeste());
     super.initState();
-  }*/
+  } */
 
   @override
   Widget build(BuildContext context) {
@@ -46,19 +47,46 @@ class _ItensState extends State<Itens> {
                   children: [
                     barraDeBusca(),
                     SizedBox(height: 20),
-                    _listas.isEmpty
-                        ? Text("Você ainda não adicionou nenhum item na sua lista '${widget.lista.nome}'")
+                    _itens.isEmpty
+                        ? Text(
+                            "Você ainda não adicionou nenhum item na sua lista '${widget.lista.nome}'")
                         : Expanded(
                             child: ListView.builder(
-                              itemCount: _listas.length,
+                              itemCount: _itens.length,
                               itemBuilder: (context, index) {
                                 return Card(
                                   child: ListTile(
+                                    leading: IconButton(
+                                      icon: Icon(_itens[index].isDone
+                                          ? Icons.check_box
+                                          : Icons.check_box_outline_blank),
+                                      onPressed: () {
+                                        setState(() {
+                                          _itens[index].isDone = !_itens[index].isDone;
+                                        });
+                                      },
+                                    ),
+                                    trailing: IconButton(
+                                      icon: Icon(
+                                          Icons.delete_sharp),
+                                      onPressed: () {
+                                        setState(() {
+                                          _itens.removeAt(index);
+                                        });
+                                      },
+                                    ),
+
                                     onTap: () {},
                                     title: Text(
-                                      _listas[index].nome,
+                                      _itens[index].nome!,
                                       style: TextStyle(
                                         fontSize: 10,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                    subtitle: Text(
+                                      _itens[index].qtde!,
+                                      style: TextStyle(
                                         color: Colors.black,
                                       ),
                                     ),
@@ -66,7 +94,7 @@ class _ItensState extends State<Itens> {
                                 );
                               },
                             ),
-                          )
+                          ),
                   ],
                 ),
               ),
@@ -75,34 +103,148 @@ class _ItensState extends State<Itens> {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: criarItem,
         child: Icon(Icons.add),
       ),
+    );
+  }
+
+  salvarItem() {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        _itens.add(Item(nome: _nomeItem.text, qtde: _qtdItem.text));
+        _nomeItem.clear();
+        _qtdItem.clear();
+      });
+      Navigator.pop(context);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Item adicionado com sucesso"),
+        ),
+      );
+    }
+  }
+
+  criarItem() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(10.0),
+            ),
+          ),
+          title: Text(
+            "Adicionar Item",
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          content: Container(
+            height: 320,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  SizedBox(height: 20),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(2, 0, 0, 2),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4),
+                      color: Colors.deepPurple[50],
+                    ),
+                    child: TextFormField(
+                      controller: _nomeItem,
+                      decoration: InputDecoration(
+                        labelText: 'Digite o nome do item',
+                        icon: Icon(Icons.abc),
+                        border: InputBorder.none,
+                      ),
+                      validator: (nome) {
+                        if (nome == null || nome.isEmpty) {
+                          return 'Por favor, digite o nome do item';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(2, 0, 0, 2),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4),
+                      color: Colors.deepPurple[50],
+                    ),
+                    child: TextFormField(
+                      controller: _qtdItem,
+                      decoration: InputDecoration(
+                        labelText: 'Digite a quantidade do item',
+                        hintText:
+                            'Você pode digitar também a unidade de medida',
+                        icon: Icon(Icons.pin),
+                        border: InputBorder.none,
+                      ),
+                      validator: (qtd) {
+                        if (qtd == null || qtd.isEmpty) {
+                          return 'Por favor, digite a quantidade do item';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 32),
+                  ElevatedButton(
+                    onPressed: salvarItem,
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(110, 40),
+                      backgroundColor: Colors.purple[400],
+                    ),
+                    child: Text(
+                      "Criar",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(
+                      "Cancelar",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
 
 Widget barraDeBusca() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: TextField(
-        decoration: InputDecoration(
-          contentPadding: EdgeInsets.all(0),
-          prefixIcon: Icon(
-            Icons.search,
-            size: 20,
-          ),
-          prefixIconConstraints: BoxConstraints(
-            maxHeight: 20,
-            minWidth: 20,
-          ),
-          border: InputBorder.none,
-          hintText: 'Buscar',
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 10),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(20),
+    ),
+    child: TextField(
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(0),
+        prefixIcon: Icon(
+          Icons.search,
+          size: 20,
         ),
+        prefixIconConstraints: BoxConstraints(
+          maxHeight: 20,
+          minWidth: 20,
+        ),
+        border: InputBorder.none,
+        hintText: 'Buscar',
       ),
-    );
-  }
+    ),
+  );
+}
