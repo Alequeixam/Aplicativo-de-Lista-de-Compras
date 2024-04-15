043153e9cc9e5e28dbef8c2397b486ab947d25d5
Alex Santana <alequeixam@gmail.com>
2024-04-14 21:04:37 -0300

    tela sobre com link externo

--- app_lista_compras/lib/view/sobre_view.dart
+++ app_lista_compras/lib/view/sobre_view.dart
@@ -1,9 +1,14 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class Sobre extends StatelessWidget {
-  const Sobre({super.key});
+  Sobre({super.key});
+
+  final String _urlGitHub = "https://github.com/Alequeixam/";
 
   @override
   Widget build(BuildContext context) {
@@ -17,21 +22,36 @@ class Sobre extends StatelessWidget {
         child: Padding(
           padding: EdgeInsets.all(20),
           child: Column(
-            children: const [
+            children: [
               Text("Lista de Compras",
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
               SizedBox(height: 40),
               Text(
                 "O aplicativo de Lista de Compras é projetado para te ajudar a criar e gerenciar listas de compras de forma eficiente. Ele permite que você adicione itens à uma lista, edite um item, marque itens como comprados e remova itens conforme necessário.",
                 textAlign: TextAlign.justify,
+                style: TextStyle(fontSize: 17),
               ),
               Expanded(
                   child: Align(
                 alignment: FractionalOffset.bottomCenter,
-                child: Row(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Text("Dev: Alex Henrique Santana"),
-                    Icon(Icons.)
+                    Text("Devs:"),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Alex Henrique Santana"),
+                        SizedBox(width: 5),
+                        InkWell(
+                          onTap: () => launchUrlString("https://github.com/Alequeixam/"),
+                          
+                          child: FaIcon(FontAwesomeIcons.github),
+                          
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               )),
